9e4644ffc4da57b1cbf813bcfe1199edcd1df67f
rebanks <rebanks@w2odigital.com>
2013-09-21 13:40:54 -0500

    Changed ElasticSearchStateFactory to be compatible with storm version 0.8.1

--- src/main/java/com/hmsonline/storm/elasticsearch/trident/ElasticSearchStateFactory.java
+++ src/main/java/com/hmsonline/storm/elasticsearch/trident/ElasticSearchStateFactory.java
@@ -5,7 +5,7 @@ package com.hmsonline.storm.elasticsearch.trident;
 
 import java.util.Map;
 
-import backtype.storm.task.IMetricsContext;
+//import backtype.storm.task.IMetricsContext;
 import storm.trident.state.State;
 import storm.trident.state.StateFactory;
 
@@ -16,10 +16,19 @@ public class ElasticSearchStateFactory implements StateFactory {
      */
     private static final long serialVersionUID = -6363900741883372326L;
 
+    @Override
+    public State makeState(Map conf, int i, int i2) {
+        return new ElasticSearchState(conf);
+    }
+
+    /*
+
+    Comment out code is for version 0.8.2
+
     @SuppressWarnings("rawtypes")
     @Override
     public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
         return new ElasticSearchState(conf);
     }
-
+    */
 }
